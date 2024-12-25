0c360d457e92480739c55d6c15090e7d1e23d75d
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 09:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function createTable() {
-    //Write your code here
-  
-}
+ function createTable() {
+        //Write your code here
+        let table = document.querySelector("table");
+
+        let rowN = prompt("inputNumber of rows");
+        let colN = prompt("inputNumber of columns");
+        let tbody = document.createElement("tbody");
+        table.append(tbody);
+
+        for (let i = 0; i < rowN; i++) {
+          let row = document.createElement("tr");
+          tbody.append(row);
+
+          for (let j = 0; j < colN; j++) {
+            let col = document.createElement("td");
+            col.innerText = `Row-${i} Column-${j}`;
+            row.append(col);
+          }
+        }
+      }
